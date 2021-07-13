4221d2bed7cb14d10f1a187692a5d6d73ca8b6f5
Kumar Saurabh Raj <kumarsaurabhraj.sr@gmail.com>
2021-07-13 16:20:49 +0530

    Purge old architecture

--- src/Contexts/CollisionContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from 'react';
-
-export const CollisionContext = createContext({});
--- src/model/BlocksModel/BlockSvg/FlowBlockSVG.js
+++
@@ -1,5 +0,0 @@
-export default class FlowBlockSVG {
-    static NOTCH_WIDTH = 5;
-    static NOTCH_HEIGHT = 2;
-    static NOTCH_DISTANCE = 5;
-}
